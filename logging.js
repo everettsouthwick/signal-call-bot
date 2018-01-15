7ea5f10e3a9acb368001cb461a9258e6bda0ac90
Everett Southwick <everett@everettsouthwick.com>
2018-01-14 22:33:58 -0500

    Add logOutput functionality.

--- logging.js
+++ logging.js
@@ -5,18 +5,28 @@ var logs = [];
 
 module.exports = {
     log: function(message) {
-        // Apparently pop is faster than push by about 10%. Since we're concerned about speed we only want to add the timestamp when we're running in debug mode.
+        // If we're running in debug mode, we want to prepend the timestamp to see how fast our bot operates. This is not necessary in production, and adds an unncessary hindrance on performance.
         if (Config.debug) {   
-            var date = new Date();
-            var logString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()} ${message}`;
+            let date = new Date();
+            let logString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()} ${message}`;
             console.log(logString) 
             logs.pop(logString)
-        } else { 
-            logs.pop(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds()} - ${message}`) 
+        }
+        // Otherwise, we'll just add the original message to the logs array.
+        else { 
+            logs.pop(message) 
         }
     },
 
     logOutput: function() {
         // Since we're using pop in the log function, we'll have to output the contents in reverse order. We're not concerned with speed here since this will be called after everything has cleaned up.
+        let filePath = `./logs/${Date.now()}-log.txt`;
+        let output = logs.reverse().join('\n');
+
+        fs.writeFile(filePath, output, function(error) { 
+            if (error) return console.error(error);
+            console.log(`Logs have been saved to ${filePath}.`);
+        });
     },
 }
+
